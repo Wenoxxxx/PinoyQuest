9a6bd0bf5f1d00ce10cd7dcf3f60a15a22c45bdf
Wenox <145521738+Wenox26@users.noreply.github.com>
2025-10-11 22:11:46 +0800

    Entity: Added general entity settings

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -8,7 +8,15 @@ public class Entity {
     public int x, y;
     public int speed;
 
-    
+    public BufferedImage up1, down1, left1, right1;
+    public String direction;
+
+    // UPDATE ENTITY BEHAVIOR (TO BE OVERIDDEN)
+    public void update(){}
+
+
+    // DRAW ENTITY SPRITE (TO BE OVERIDDEN)
+    public void draw(Graphics2D g2){}
 
 
 }
