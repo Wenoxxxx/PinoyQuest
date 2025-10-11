4b6dcd111e1011c815fe617b9884d2f05ddb4f04
Wenox <145521738+Wenox26@users.noreply.github.com>
2025-10-11 21:52:01 +0800

    Entity: Entity and Player created

---
+++ src/entity/Entity.java
@@ -0,0 +1,14 @@
+package src.entity;
+
+import java.awt.image.BufferedImage;
+import java.awt.Graphics2D;
+
+public class Entity {
+    
+    public int x, y;
+    public int speed;
+
+    
+
+
+}
---
+++ src/entity/Player.java
@@ -0,0 +1,13 @@
+package src.entity;
+
+import javax.swing.plaf.basic.BasicComboBoxUI.KeyHandler;
+import src.core.GamePanel;
+
+public class Player extends Entity {
+    
+    GamePanel gamePanel;
+    KeyHandler keyHandler;
+
+
+
+}
