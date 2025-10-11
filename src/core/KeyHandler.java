1acb0bd2c2279e06fb4a4f559c91cc13ce985a1b
Wenox <145521738+Wenox26@users.noreply.github.com>
2025-10-11 15:59:36 +0800

    Key Handler: Added Key Movements

--- src/core/KeyHandler.java
+++ src/core/KeyHandler.java
@@ -1,5 +1,59 @@
 package src.core;
 
-public class KeyHandler {
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+
+
+public class KeyHandler implements KeyListener {
+    
+
+    public boolean upPressed, downPressed, leftPressed, rightPressed;
+
+
+
+    // DETECT TYPED CHARACTERS
+    @Override
+    public void keyTyped(KeyEvent e){}
+
+
+
+    // KEY MOVEMENT:  DETECT IF PRESSED
+    @Override
+    public void keyPressed(KeyEvent e){
+
+        int code = e.getKeyCode();// Get the integer of the pressed key
+
+        if (code == KeyEvent.VK_W) upPressed = true;
+        if (code == KeyEvent.VK_S) downPressed = true;
+        if (code == KeyEvent.VK_A) leftPressed = true;
+        if (code == KeyEvent.VK_D) rightPressed = true;
+
+    }
+
+    // KEY MOVEMENT: DETECT IF RELEASED
+    @Override
+    public void keyReleased(KeyEvent e){
+
+        int code = e.getKeyCode();// Get the integer of the pressed key
+
+        if (code == KeyEvent.VK_W) upPressed = false;
+        if (code == KeyEvent.VK_S) downPressed = false;
+        if (code == KeyEvent.VK_A) leftPressed = false;
+        if (code == KeyEvent.VK_D) rightPressed = false;
+
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
     
 }
