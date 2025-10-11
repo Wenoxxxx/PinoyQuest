20039899d68247caba162236cb839a13a4b15ee2
Wenox <145521738+Wenox26@users.noreply.github.com>
2025-10-11 19:46:46 +0800

    Main: [JFRAME] Screen set to full screen

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,26 +5,30 @@ import src.core.GamePanel;
 
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     
     public static void main(String[] args) {
         
         //MAIN GAME FRAME 
-        JFrame mainFrame = new JFrame();
+        JFrame mainFrame = new JFrame("Pinoy Quest");
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainFrame.setResizable(false);
+        mainFrame.setResizable(true);
+        mainFrame.setUndecorated(false);
 
+
+        // PUTS THE GAME SCREEN INSIDE THE WINDOW
         GamePanel gamePanel = new GamePanel();
         mainFrame.add(gamePanel);
         mainFrame.pack();
 
-        mainFrame.setLocationRelativeTo(null);
+        // FULL SCREEN
         mainFrame.setVisible(true);
+        mainFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
-
-        // START THE SEPARATE GAME LOOP
-        // gamePanel.startGame();
+        // START THE SEPARATE GAME LOOP THREAD
+        gamePanel.startGame();
 
 
     }
