a39f03104ebb8b7d7347f56ad794f758c5563728
Wenox <145521738+Wenox26@users.noreply.github.com>
2025-10-23 10:55:00 +0800

    General Setup for Entity: Player

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -3,11 +3,19 @@ package src.entity;
 import javax.swing.plaf.basic.BasicComboBoxUI.KeyHandler;
 import src.core.GamePanel;
 
+import java.awt.Graphics2D;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+
 public class Player extends Entity {
     
     GamePanel gamePanel;
     KeyHandler keyHandler;
 
-
+    public Player(KeyHandler keyHandler){
+        this,keyHandler = keyHandler;
+        setDefaultValues();
+    }
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -14,15 +14,15 @@ public class Main {
         //MAIN GAME FRAME 
         JFrame mainFrame = new JFrame("Pinoy Quest");
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainFrame.setResizable(true);
+        mainFrame.setResizable(true);//SET FALSE FOR FULL SCREEN
         mainFrame.setUndecorated(false);
-
-
+        
+        
         // PUTS THE GAME SCREEN INSIDE THE WINDOW
         GamePanel gamePanel = new GamePanel();
         mainFrame.add(gamePanel);
         mainFrame.pack();
-
+        
         // FULL SCREEN
         mainFrame.setVisible(true);
         mainFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
