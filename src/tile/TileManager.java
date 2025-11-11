cb86f0134528cb0ab1282fb077ea447c6335c0b9
Wenox <145521738+Wenox26@users.noreply.github.com>
2025-11-11 13:49:15 +0800

    Update: Test run  and tile placement

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -23,26 +23,61 @@ public class TileManager {
     }
 
     public void getTileImage() {
+        // Get project root directory and construct base path
+        String projectRoot = System.getProperty("user.dir");
+        String basePath = projectRoot + File.separator + "src" + File.separator + "assets" + File.separator + "tiles" + File.separator;
+        
         try {
             tile[0] = new Tile();
-            tile[0].image = ImageIO.read(getClass().getResourceAsStream("/res/FieldsTile_01.png"));
+            File file0 = new File(basePath + "FieldsTile_01.png");
+            if (file0.exists()) {
+                tile[0].image = ImageIO.read(file0);
+            } else {
+                System.err.println("Tile image not found: " + file0.getPath());
+            }
 
             tile[1] = new Tile();
-            tile[1].image = ImageIO.read(getClass().getResourceAsStream("/res/FieldsTile_02.png"));
+            File file1 = new File(basePath + "FieldsTile_02.png");
+            if (file1.exists()) {
+                tile[1].image = ImageIO.read(file1);
+            } else {
+                System.err.println("Tile image not found: " + file1.getPath());
+            }
 
             tile[2] = new Tile();
-            tile[2].image = ImageIO.read(getClass().getResourceAsStream("/res/FieldsTile_03.png"));
+            File file2 = new File(basePath + "FieldsTile_03.png");
+            if (file2.exists()) {
+                tile[2].image = ImageIO.read(file2);
+            } else {
+                System.err.println("Tile image not found: " + file2.getPath());
+            }
 
             tile[3] = new Tile();
-            tile[3].image = ImageIO.read(getClass().getResourceAsStream("/res/FieldsTile_04.png"));
+            File file3 = new File(basePath + "FieldsTile_04.png");
+            if (file3.exists()) {
+                tile[3].image = ImageIO.read(file3);
+            } else {
+                System.err.println("Tile image not found: " + file3.getPath());
+            }
 
             tile[4] = new Tile();
-            tile[4].image = ImageIO.read(getClass().getResourceAsStream("/res/FieldsTile_05.png"));
+            File file4 = new File(basePath + "FieldsTile_05.png");
+            if (file4.exists()) {
+                tile[4].image = ImageIO.read(file4);
+            } else {
+                System.err.println("Tile image not found: " + file4.getPath());
+            }
 
             tile[5] = new Tile();
-            tile[5].image = ImageIO.read(getClass().getResourceAsStream("/res/FieldsTile_06.png"));
+            File file5 = new File(basePath + "FieldsTile_06.png");
+            if (file5.exists()) {
+                tile[5].image = ImageIO.read(file5);
+            } else {
+                System.err.println("Tile image not found: " + file5.getPath());
+            }
 
         } catch (IOException e) {
+            System.err.println("Error loading tile images: " + e.getMessage());
             e.printStackTrace();
         }
 
@@ -50,6 +85,9 @@ public class TileManager {
 
     // helper method for tile drawings
     private void drawTileGrid(Graphics2D g2, BufferedImage tileImage, int startX, int startY, int cols, int rows) {
+        if (tileImage == null) {
+            return; // Skip drawing if tile image is null
+        }
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++) {
                 int screenX = startX + (col * gp.tileSize) - gp.cameraX;
